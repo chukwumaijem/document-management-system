7445a0c7cc9ed026925190dfc533c543ea18c5a6
Ezumezu Anselm <aezumezu@gmail.com>
2016-11-11 23:24:00 +0100

    Code refactor

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -3,11 +3,13 @@
 const jwt = require('jsonwebtoken');
 
 let authenticate = function (req, res, next) {
-  let token = req.body.token || req.query.token || req.headers['x-access-token'];
+  let token = req.body.token || req.query.token ||
+    req.headers['x-access-token'];
   if (token) {
     jwt.verify(token, process.env.secret, function (err, decoded) {
       if (err) {
-        return res.json({ success: false, message: err.message });
+        return res.status(401)
+          .json({ error: err.message });
       } else {
         req.decoded = decoded;
         next();
